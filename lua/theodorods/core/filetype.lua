c51e4b4a94acb435a1de9bed27bd9c6d7f0db6a0
theodorods <bjorntheodor.odsbu@gmail.com>
2023-11-01 12:57:08 +0100

    Fixed syntax bug

--- lua/theodorods/core/filetype.lua
+++ lua/theodorods/core/filetype.lua
@@ -1,8 +1,15 @@
 vim.cmd([[
-augroup filetypedetect
-  au!
-  au BufNewFile,BufReadPost *.mod  setf ampl
-  au BufNewFile,BufReadPost *.dat  setf ampl
-  au BufNewFile,BufReadPost *.ampl setf ampl
+augroup ampl_filetype
+   au!
+   au FileType modsim3 if expand('<afile>:e') == 'mod' | set filetype=ampl | set syntax=ampl | endif
+   au BufNewFile,BufRead *.dat  setf ampl
+   au BufNewFile,BufRead *.ampl setf ampl
+augroup END
+]])
+
+vim.cmd([[
+augroup ampl_syntax
+   au!
+   au VimEnter *.mod set syntax=ampl
 augroup END
 ]])
